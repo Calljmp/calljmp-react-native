0c266fc06ebc633d4bb12fecb552fc2015563065
Volodymyr Lykhonis <volodymyr@lykhonis.com>
2025-05-05 15:08:25 +0200

    v0.0.21-preview

--- src/secure-store.ts
+++ src/secure-store.ts
@@ -2,6 +2,8 @@ import NativeStore from './specs/NativeCalljmpStore';
 
 export type StoreKey = 'accessToken';
 
+const PREFIX = 'calljmp:store:';
+
 export class SecureStore {
   private _cache: Record<StoreKey, string | null | undefined> = {
     accessToken: undefined,
@@ -10,9 +12,9 @@ export class SecureStore {
   async put(key: StoreKey, value: string | null) {
     this._cache[key] = value;
     if (value === null) {
-      return await NativeStore.secureDelete(key);
+      return await NativeStore.secureDelete(`${PREFIX}${key}`);
     }
-    return await NativeStore.securePut(key, value);
+    return await NativeStore.securePut(`${PREFIX}${key}`, value);
   }
 
   async delete(key: StoreKey) {
@@ -22,7 +24,7 @@ export class SecureStore {
   async get(key: StoreKey): Promise<string | null> {
     const cachedValue = this._cache[key];
     if (cachedValue === undefined) {
-      const value = await NativeStore.secureGet(key);
+      const value = await NativeStore.secureGet(`${PREFIX}${key}`);
       this._cache[key] = value;
       return value;
     }
